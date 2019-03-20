710bf9aba6bb629d97e4b54469580f98af6175c6
rleongPd5 <rleong00@stuy.edu>
2019-03-20 05:46:16 -0400

    Finished up the mergesort helper, working on merge now

--- Merge.java
+++ Merge.java
@@ -8,9 +8,23 @@ public class Merge{
     if (lo >= hi){ //went through all the indices
       return;
     }
-    mergesort(data, lo, (lo + hi)/2); //split in half and mergesort left side
-    mergesort(data, (lo + hi)/2+1, hi); //split in half and mergesort right side
-    //merge
+    int mid = (lo + hi)/2;
+    int leftLength = mid - left + 1;
+    int rightLength = right - mid;
+    int[] L = new int[leftLength];
+    int[] R = new int[rightLength];
+    //copy over values
+    for (int i = 0; i < L.length; i++){
+      //left array
+      L[i]=data[i];
+    }
+    for (int i = 0; i < R.length; i++){
+      //right array
+      R[i]=data[mid+i];
+    }
+    mergesort(data, lo, mid); //split in half and mergesort left side
+    mergesort(data, mid+1, hi); //split in half and mergesort right side
+    merge(data, L, R, mid); //merge them back together
   }
   /*
   Pseudocode:
@@ -23,6 +37,26 @@ public class Merge{
   */
   //You can make arrays as temporary space if you wish! This is the easier method, but will be a bit slower.
 
+  private static void merge(int[] data, int[] L, int[] R, int mid){
+    // start with creating temp arrays
+    /*int leftLength = mid - left + 1;
+    int rightLength = right - mid;
+    int[] L = new int[leftLength];
+    int[] R = new int[rightLength];
+    */
+    //store indices
+    int i = 0;
+    int l = 0;
+    int r = 0;
+    //loop thru the elements in one side and elements on the other and move it into the right place
+    for (int r = 0; r < rightLength; r++){
+      for (int l = 0; l < leftLength; l++){
+        if (L[l] < R[r]){
+
+        }
+      }
+    }
+  }
   //If you want to make it faster:
   //Have your mergesort take both the data and temp arrays. Mergesort the temp, and merge into the original!
   //e.g.
